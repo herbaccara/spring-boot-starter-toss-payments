198b0298a325910f3960cd2713daeb69123c8192
allbegray <allbegray@nate.com>
2023-02-25 23:41:15 +0900

    :recycle: TossPaymentsWebMvcConfigurer 를 TossPaymentsAutoConfiguration 내로 이동

--- src/main/kotlin/herbaccara/boot/autoconfigure/toss/payments/TossPaymentsAutoConfiguration.kt
+++ src/main/kotlin/herbaccara/boot/autoconfigure/toss/payments/TossPaymentsAutoConfiguration.kt
@@ -4,17 +4,22 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import herbaccara.toss.payments.TossPaymentsService
+import herbaccara.toss.payments.WebhookController
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.nio.charset.StandardCharsets
 import java.util.*
 
@@ -22,6 +27,18 @@ import java.util.*
 @EnableConfigurationProperties(TossPaymentsProperties::class)
 @ConditionalOnProperty(prefix = "toss.payments", value = ["enabled"], havingValue = "true")
 class TossPaymentsAutoConfiguration {
+
+    @AutoConfiguration
+    @ConditionalOnClass(value = [WebMvcConfigurer::class])
+    @ConditionalOnProperty(prefix = "toss.payments.webhook", value = ["enabled"], havingValue = "true")
+    class TossPaymentsWebMvcConfigurer : WebMvcConfigurer {
+
+        @Bean
+        @ConditionalOnMissingBean(WebhookController::class)
+        fun webhookController(applicationEventPublisher: ApplicationEventPublisher): WebhookController {
+            return WebhookController(applicationEventPublisher)
+        }
+    }
 
     @Bean("tossPaymentsObjectMapper")
     fun objectMapper(properties: TossPaymentsProperties): ObjectMapper {
