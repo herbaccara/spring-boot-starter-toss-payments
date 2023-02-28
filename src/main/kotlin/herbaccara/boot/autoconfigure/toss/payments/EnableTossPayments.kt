789fd9b079fa47ade251ebf4e2bc62014d293bf9
이용홍 <allbegray.unimewo@gmail.com>
2023-02-28 18:01:14 +0900

    :bug: @EnableTossPayments 타겟 변경

--- src/main/kotlin/herbaccara/boot/autoconfigure/toss/payments/EnableTossPayments.kt
+++ src/main/kotlin/herbaccara/boot/autoconfigure/toss/payments/EnableTossPayments.kt
@@ -3,7 +3,7 @@ package herbaccara.boot.autoconfigure.toss.payments
 import org.springframework.context.annotation.Import
 import java.lang.annotation.*
 
-@Target
+@Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Import(TossPaymentsAutoConfiguration::class)
